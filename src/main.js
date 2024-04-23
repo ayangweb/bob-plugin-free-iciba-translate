b4904ee038c80ab4999addb4b678eec52467658d
ayang <473033518@qq.com>
2024-04-23 11:22:46 +0800

    refactor: 优化了在 macos14 无法正常使用插件的问题

--- src/main.js
+++ src/main.js
@@ -35,56 +35,56 @@ function decryptContent(content) {
 	return JSON.parse(CryptoJS.enc.Utf8.stringify(result));
 }
 
-async function translate(query, completion) {
-	try {
-		const { text: q, detectFrom, detectTo } = query;
+function translate(query, completion) {
+	(async () => {
+		try {
+			const { text: q, detectFrom, detectTo } = query;
 
-		// 获取请求参数中的语种
-		const getLanguage = (detect) =>
-			config.languages.find((language) => language[0] === detect)[1];
+			// 获取请求参数中的语种
+			const getLanguage = (detect) =>
+				config.languages.find((language) => language[0] === detect)[1];
 
-		const from = getLanguage(detectFrom);
+			const from = getLanguage(detectFrom);
 
-		const to = getLanguage(detectTo);
+			const to = getLanguage(detectTo);
 
-		const sign = getSign(q);
+			const sign = getSign(q);
 
-		const result = await $http.post({
-			url: `https://ifanyi.iciba.com/index.php?c=trans&m=fy&client=6&auth_user=key_web_new_fanyi&sign=${sign}`,
-			body: {
-				q,
-				from,
-				to,
-			},
-			header: {
-				"Content-Type": "application/x-www-form-urlencoded",
-			},
-		});
+			const result = await $http.post({
+				url: `https://ifanyi.iciba.com/index.php?c=trans&m=fy&client=6&auth_user=key_web_new_fanyi&sign=${sign}`,
+				body: {
+					q,
+					from,
+					to,
+				},
+				header: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+			});
 
-		if (!result?.data) throw new Error();
+			if (!result?.data) throw new Error();
 
-		const { error_code, message, content } = result.data;
+			const { error_code, message, content } = result.data;
 
-		if (error_code) throw new Error(message);
+			if (error_code) throw new Error(message);
 
-		completion({
-			result: {
-				from,
-				to,
-				toParagraphs: decryptContent(content).out.split("\n"),
-			},
-		});
-	} catch ({ message }) {
-		completion({
-			error: {
-				type: "unknown",
-				message,
-			},
-		});
-	}
+			completion({
+				result: {
+					from,
+					to,
+					toParagraphs: decryptContent(content).out.split("\n"),
+				},
+			});
+		} catch ({ message }) {
+			completion({
+				error: {
+					type: "unknown",
+					message,
+				},
+			});
+		}
+	})();
 }
 
-module.exports = {
-	supportLanguages,
-	translate,
-};
+exports.supportLanguages = supportLanguages;
+exports.translate = translate;
